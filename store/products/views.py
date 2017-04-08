a78d8291e86f79fbb5099d612d9460f7a10a7c5f
Alexander Zaytsev <thebestzorro@yandex.ru>
2017-04-08 23:58:08 +0300

    boostrap js/css

--- store/products/views.py
+++ store/products/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+# coding: utf8
 
-# Create your views here.
+from logging import getLogger
+
+from django.shortcuts import render
+from django.http import HttpRequest
+
+logger = getLogger(__name__)
+
+
+def index(request: HttpRequest):
+    context = {'a': 1, 'b': 'Hi All!'}
+    return render(request, 'products/index.html', context)
--- store/store/urls.py
+++ store/store/urls.py
@@ -18,12 +18,16 @@ from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.contrib import admin
 
+from products.views import index
+
 urlpatterns = [
     # url(r'^products/', include('products.urls')),
+    url(r'^$', index, name='index'),
     url(r'^admin/', admin.site.urls),
 ]
 
 if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
     import debug_toolbar

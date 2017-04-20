ed89488ff086d5822e18e8e7552dfa6475341145
Alexander Zaytsev <thebestzorro@yandex.ru>
2017-04-20 22:47:58 +0300

    new test

--- store/products/tests.py
+++ store/products/tests.py
@@ -2,6 +2,8 @@
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 
+from libs.utils import batch_split
+
 
 class TestIndex(TestCase):
 
@@ -9,3 +11,19 @@ class TestIndex(TestCase):
         url = reverse('index')
         resp = self.client.get(url)
         self.assertEqual(resp.status_code, 200)
+
+
+class TestUtils(TestCase):
+
+    def test_batch_split(self):
+        examples = {
+            tuple(): [],
+            (1,): [(1,)],
+            (1, 2): [(1, 2)],
+            (1, 2, 3): [(1, 2), (3,)],
+            (1, 2, 3, 4): [(1, 2), (3, 4)],
+            (1, 2, 3, 4, 5): [(1, 2), (3, 4), (5,)],
+            (1, 2, 3, 4, 5, 6): [(1, 2), (3, 4), (5, 6)],
+        }
+        for item, expected in examples.items():
+            self.assertEqual(batch_split(item, 2), expected)
